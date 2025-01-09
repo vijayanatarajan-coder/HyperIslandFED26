cbebc86182e08a0ba0626c0f778faae77f59291c
vijayanatarajan-coder <179537302+vijayanatarajan-coder@users.noreply.github.com>
2025-01-09 12:02:39 +0100

    script is uopdated

--- 5/script.js
+++ 5/script.js
@@ -1,30 +1,45 @@
-// Base URL
 const apiUrl = "https://api.example.com/data";
 
-// Function to make a GET request using
-// TODO: 1. find the error and fix it, 2. finish the function
-function fetchData(url) {
-  fetch(url)
+function fetchData(apiUrl) {
+  fetch(apiUrl)
     .then((response) => {
-      if (response.ok) {
+      if (!response.ok) {
         throw new Error("Network response was not ok " + response.statusText);
       }
       return response.json();
     })
     .then((data) => {
       console.log(data);
+    })
+    .catch((error) => {
+      console.error("There was a problem with the fetch operation:", error);
     });
 }
-
 fetchData(apiUrl);
 
-// Function to make a POST request
-// TODO: Implement the function
-function postData(url, data) {}
-
+function postData(apiUrl, data) {
+  fetch(apiUrl, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  })
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("Network response was not ok " + response.statusText);
+      }
+      return response.json();
+    })
+    .then((responseData) => {
+      console.log("Posted data successfully:", responseData);
+    })
+    .catch((error) => {
+      console.error("There was a problem with the post operation:", error);
+    });
+}
 const dataToPost = {
   name: "John Doe",
   age: 21,
 };
-
 postData(apiUrl, dataToPost);
