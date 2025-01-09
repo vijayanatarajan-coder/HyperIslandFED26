f092f92945803c1c2228aa9b497dc0f4157ca134
Ryan <ryjen.pears@gmail.com>
2025-01-09 09:57:39 +0100

    assignment 6added

---
+++ 6/script.js
@@ -0,0 +1,15 @@
+// TODO: Set item in localStorage
+
+// TODO: Get item from localStorage
+
+// TODO: Remove item from localStorage
+
+// TODO: Clear all items from localStorage
+
+// TODO: Set item in sessionStorage
+
+// TODO: Get item from sessionStorage
+
+// TODO: Remove item from sessionStorage
+
+// TODO: Clear all items from sessionStorage
