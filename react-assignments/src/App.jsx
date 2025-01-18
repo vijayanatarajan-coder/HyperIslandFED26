d749e7f97c2969924caf5fa12ca27d3dd0216471
vijayanatarajan-coder <179537302+vijayanatarajan-coder@users.noreply.github.com>
2025-01-18 18:58:30 +0100

    assignment 9 done

--- react-assignments/src/App.jsx
+++ react-assignments/src/App.jsx
@@ -1,8 +1,9 @@
 import "./App.css";
-import Person from "./Person";
+//import Person from "./Components/Person";
+import MyStateComponent from "./Components/MyStateComponent/MyStateComponent";
 
 function App() {
-  const person = {
+  /*const person = {
     name: "John",
     info: {
       age: 25,
@@ -10,15 +11,13 @@ function App() {
       job: "Developer",
       hobbies: ["reading", "coding", "gaming"],
     },
-  };
+  };*/
 
   return (
     <div>
-      <header>
-        <h1>Person</h1>
-      </header>
       {/* TODO create a child component in proper jsx, the component takes the person object as props and render the data */}
-      <Person props={person} />
+      {/*<Person props={person} />*/}
+      <MyStateComponent />
     </div>
   );
 }
--- react-assignments/src/Components/MyStateComponent/MyStateComponent.jsx
+++ react-assignments/src/Components/MyStateComponent/MyStateComponent.jsx
@@ -1,11 +1,56 @@
 import React, { useState } from "react";
+import "./MyStateComponent.css";
 
 const MyStateComponent = () => {
-  // TODO create a form using useState hook, the form should have two input fields, one for name and one for age, and a submit button and handle form data. Submit/console.log the form data.
+  const [inputValue, setInputValue] = useState({
+    name: "",
+    age: "",
+  });
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setInputValue((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    if (!inputValue.name || !inputValue.age) {
+      alert("Please fill out both fields.");
+      return;
+    }
+    console.log(`Form submitted with input: ${JSON.stringify(inputValue)}`);
+
+    setInputValue({
+      name: "",
+      age: "",
+    });
+  };
+
   return (
-    <div>
-      <h1>My State Component</h1>
-    </div>
+    <form onSubmit={handleSubmit} className="form-container">
+      <h1>Form</h1>
+      <label>
+        Name
+        <input
+          type="text"
+          name="name"
+          value={inputValue.name}
+          onChange={handleInputChange}
+        />
+      </label>
+      <label>
+        Age
+        <input
+          type="number"
+          name="age"
+          value={inputValue.age}
+          onChange={handleInputChange}
+        />
+      </label>
+      <button type="submit">Submit</button>
+    </form>
   );
 };
 
